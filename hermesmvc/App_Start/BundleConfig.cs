f8a150a2fc39ef6a3c1e89e08eb7bfe4168d0b80
Bulat <darts13579@gmail.com>
2019-07-23 19:37:53 +0400

    Added datepicker to Promotions/Edit

--- hermesmvc/App_Start/BundleConfig.cs
+++ hermesmvc/App_Start/BundleConfig.cs
@@ -9,7 +9,12 @@ namespace hermesmvc
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerycss").Include(                        
+                        "~/Content/themes/base/jquery-ui.css",
+                        "~/Content/themes/base/all.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
