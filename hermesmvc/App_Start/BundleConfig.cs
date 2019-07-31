56ebd5e51d96cf2984dade35ea8ae9f59193f733
Bulat <darts13579@gmail.com>
2019-07-31 16:32:34 +0400

    firstweek complete

--- hermesmvc/App_Start/BundleConfig.cs
+++ hermesmvc/App_Start/BundleConfig.cs
@@ -10,11 +10,7 @@ namespace hermesmvc
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquerycss").Include(                        
-                        "~/Content/themes/base/jquery-ui.css",
-                        "~/Content/themes/base/all.css"));
+                        "~/Scripts/jquery-ui-{version}.js"));            
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,13 +20,17 @@ namespace hermesmvc
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap-spacelab.css",
+                      "~/Content/site.css",
+                      "~/Content/themes/base/jquery-ui.css",
+                      "~/Content/themes/base/all.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-spacelab.css",
-                      "~/Content/site.css"));
+            
         }
     }
 }
