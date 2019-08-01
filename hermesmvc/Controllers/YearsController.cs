e196d4a071c50ad17ccac39aed600b2033d34c20
Bulat <darts13579@gmail.com>
2019-08-01 14:08:48 +0400

    added Years pages

---
+++ hermesmvc/Controllers/YearsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using hermesmvc.Models;
+
+namespace hermesmvc.Controllers
+{
+    public class YearsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Years
+        public ActionResult Index()
+        {
+            return View(db.Years.ToList());
+        }
+
+        // GET: Years/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Year year = db.Years.Find(id);
+            if (year == null)
+            {
+                return HttpNotFound();
+            }
+            return View(year);
+        }
+
+        // GET: Years/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Years/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name")] Year year)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Years.Add(year);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(year);
+        }
+
+        // GET: Years/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Year year = db.Years.Find(id);
+            if (year == null)
+            {
+                return HttpNotFound();
+            }
+            return View(year);
+        }
+
+        // POST: Years/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name")] Year year)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(year).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(year);
+        }
+
+        // GET: Years/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Year year = db.Years.Find(id);
+            if (year == null)
+            {
+                return HttpNotFound();
+            }
+            return View(year);
+        }
+
+        // POST: Years/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Year year = db.Years.Find(id);
+            db.Years.Remove(year);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
